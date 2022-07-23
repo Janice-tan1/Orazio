b44eaea8bb0d54dc1bd1e8571265bc90bd71d2a4
Xinleeee <109804291+Xinleeee@users.noreply.github.com>
2022-07-23 20:18:57 +0800

    Update mainSolution.py

--- Orazio/mainSolution.py
+++ Orazio/mainSolution.py
@@ -2,3 +2,9 @@ hello
 can yall see
 yes can see
 hi
+import requests
+url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=W677J7965EU3DEBN"
+response = requests.get(url)
+details = response.json()
+for info in details:
+    conversion_rate = f"USD1 = SGD{details[info]['5. Exchange Rate']}"
